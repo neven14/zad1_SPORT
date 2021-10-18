7a9f3318f005ea5a21b3347ebf918e513b4e96e6
Neven <58472164+neven14@users.noreply.github.com>
2021-10-18 09:19:19 +0200

    Add files via upload

---
+++ Zad1_Sport/Zad1_Sport/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1_Sport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport nog= new Sport("Nogomet", true, 11);
+            Sport vater = new Sport("Vaterpolo", true, 7);
+            Sport curl = new Sport("Curling",true, 5);
+        }
+    }
+}
---
+++ Zad1_Sport/Zad1_Sport/Sport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1_Sport
+{
+    public class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+        public Sport(string naziv, bool igraLiSeLoptom, byte brojIgraca)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+            BrojIgraca = brojIgraca;
+        }
+    }
+}
